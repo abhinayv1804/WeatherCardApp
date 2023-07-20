fa511451118b948ad40249777dc890cd1d686639
abhinayv1804 <135096266+abhinayv1804@users.noreply.github.com>
2023-07-20 14:16:00 +0530

    added utility

--- src/Utility.js
+++ src/Utility.js
@@ -1,6 +1,9 @@
+import React from "react";
 
 const getDayFromDate = (date) => {
-
-} 
-
+   const dateobj = new Date(date);
+   const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+   console.log(dateobj);
+   return days[dateobj.getDay()];
+}; 
 export default getDayFromDate;
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import WeatherImage from '../images/weather1.png';
 import WeatherImage2 from '../images/weather2.jpg';
+import getDayFromDate from '../Utility.js';
 
 const weather1days = ["monday","wednesday","friday"];
 const WeatherCard = (props) => {
+  const day = getDayFromDate(props.date);
   return (
     <div className="card">
-      <img className='card__image' src={weather1days.includes(props.date.toLowerCase()) ? WeatherImage : WeatherImage2} title="loading"/>
-      <h2 className="card__title">{props.temp}°C<br />{props.date}</h2>
+      <img className='card__image' src={weather1days.includes(day.toLowerCase()) ? WeatherImage : WeatherImage2} title="loading"/>
+      <h2 className="card__title">{props.temp}°C<br />{day}</h2>
       <div className='card__bottomBar'>
-        <h2 className="card__day">{props.date}</h2>
+        <h2 className="card__day">{day}</h2>
         <h2 className="card__humid">{props.humidity}</h2>
       </div>
     </div>
